34004d4155b77f09f15b326cf08263d75f5ae825
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-08-05 02:41:38 +0700

    feat: add cors middleware

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,9 +1,11 @@
 import express from "express"
+import cors from "cors"
 import { publicRouter } from "../route/public-api"
 import { errorMiddleware } from "../middleware/error-widdleware"
 import { apiRouter } from "../route/api"
 
 export const web = express()
+web.use(cors)
 web.use(express.json())
 web.use(publicRouter)
 web.use(apiRouter)
